0770467e742e183c3231883cdcb3ab8355ef36fc
norphan <nicole.orphan@slu.edu>
2023-11-30 22:28:39 -0600

    Updated test cases

--- app/src/test/java/connectFour/AppTest.java
+++ app/src/test/java/connectFour/AppTest.java
@@ -26,7 +26,7 @@ public class AppTest {
     }
 
     @Test
-    public void checkDiagonalWin(){
+    public void checkDiagonalWinSlopeDown(){
         ConnectFourGrid grid = new ConnectFourGrid();
         grid.placePiece(ConnectFourPiece.Y, 0);
         grid.placePiece(ConnectFourPiece.Y, 0);
@@ -41,6 +41,21 @@ public class AppTest {
     }
 
     @Test
+    public void checkDiagonalWinSlopeUp(){
+        ConnectFourGrid grid = new ConnectFourGrid();
+        grid.placePiece(ConnectFourPiece.Y, 6);
+        grid.placePiece(ConnectFourPiece.Y, 6);
+        grid.placePiece(ConnectFourPiece.Y, 6);
+        grid.placePiece(ConnectFourPiece.Y, 5);
+        grid.placePiece(ConnectFourPiece.Y, 5);
+        grid.placePiece(ConnectFourPiece.Y, 4);
+        for(int i=3; i<7; i++){
+            grid.placePiece(ConnectFourPiece.R, i);
+        }
+        assertTrue(grid.getWinner() == ConnectFourPiece.R);
+    }
+
+    @Test
     public void checkNoWinner(){
         ConnectFourGrid grid = new ConnectFourGrid();
         grid.placePiece(ConnectFourPiece.Y, 0);
@@ -48,5 +63,35 @@ public class AppTest {
         grid.placePiece(ConnectFourPiece.Y, 0);
         grid.placePiece(ConnectFourPiece.R, 0);
         assertTrue(grid.getWinner() == null);
+    }
+
+    @Test
+    public void checkGameOverFalse(){
+        ConnectFourGrid grid = new ConnectFourGrid();
+        grid.placePiece(ConnectFourPiece.Y, 0);
+        assertFalse(grid.isGameOver());
+    }
+
+    @Test
+    public void checkGameOverTrueWinner(){
+        ConnectFourGrid grid = new ConnectFourGrid();
+        for(int i=0; i<5; i++){
+            grid.placePiece(ConnectFourPiece.R, i);
+        }
+        assertTrue(grid.isGameOver());
+    }
+
+    @Test 
+    public void checkGameOverTrueBoardFull(){
+        ConnectFourGrid grid = new ConnectFourGrid();
+        for(int i=0; i<7; i++){
+            for(int j=0; j<3; j++){
+                grid.placePiece(ConnectFourPiece.R, i);
+            }
+            for(int j=0; j<3; j++){
+                grid.placePiece(ConnectFourPiece.Y, i);
+            }
+        }
+        assertTrue(grid.isGameOver());
     }
 }
